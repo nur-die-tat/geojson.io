15bd5a8d586e683d89f3901c2f6e5a026da1c978
simonseyock <simonseyock@googlemail.com>
2017-11-23 08:30:55 +0100

    table wip

--- src/panel/table.js
+++ src/panel/table.js
@@ -7,6 +7,7 @@ module.exports = function(context) {
         selection.html('');
 
         function rerender() {
+            // https://codepen.io/ashblue/pen/mCtuA
             var geojson = context.data.get('map');
             var props;
 
@@ -20,27 +21,81 @@ module.exports = function(context) {
             } else {
                 props = geojson.geometry ? [geojson.properties] :
                     geojson.features.map(getProperties);
-                selection.select('.blank-banner').remove();
-                selection
-                    .data([props])
-                    .call(metatable()
-                        .on('change', function(row, i) {
-                            var geojson = context.data.get('map');
-                            if (geojson.geometry) {
-                                geojson.properties = row;
-                            } else {
-                                geojson.features[i].properties = row;
-                            }
-                            context.data.set('map', geojson);
-                        })
-                        .on('rowfocus', function(row, i) {
-                            var bounds = context.mapLayer.getBounds();
-                            var j = 0;
-                            context.mapLayer.eachLayer(function(l) {
-                                if (i === j++) smartZoom(context.map, l, bounds);
-                            });
-                        })
-                    );
+
+                let cols = []
+
+                for (let prop of props) {
+                  for (let attr of Object.keys(prop)) {
+                      let type = typeof prop[attr];
+                      if (!cols.filter(c => c.name === attr && c.type === type).length) {
+                          cols.push({ name: attr, type })
+                      }
+                  }
+                }
+
+                let container = document.createElement('div');
+
+                let button = document.createElement('button');
+                button.innerHTML = 'add row';
+                container.appendChild(button);
+
+                let table = document.createElement('table');
+                let tr = document.createElement('tr');
+                for (let col of cols) {
+                    let th = document.createElement('th');
+                    th.innerHTML = col.name + ' (' + col.type + ')';
+                    tr.appendChild(th);
+                }
+                table.appendChild(tr);
+
+                for (let prop of props) {
+                    let tr = document.createElement('tr');
+                    for (let col of cols) {
+                      let td = document.createElement('td');
+                      td.setAttribute('contenteditable', true);
+                      td.innerHTML = prop[col.name];
+                      td.addEventListener('change', () => {
+                        var geojson = context.data.get('map');
+                        if (geojson.geometry) {
+                            geojson.properties[col.name] = convert(td.textContent, col.type);
+                        } else {
+                            geojson.features[i].properties = row;
+                        }
+                        context.data.set('map', geojson);
+                      })
+                      tr.appendChild(td);
+                    }
+                    table.appendChild(tr);
+                }
+
+                container.appendChild(table);
+
+                container.addEventListener('click', () => console.log('asd'));
+
+              selection[0][0].appendChild(container);
+
+                //
+                // selection.select('.blank-banner').remove();
+                // selection
+                //     .data([props])
+                //     .call(metatable()
+                //         .on('change', function(row, i) {
+                //             var geojson = context.data.get('map');
+                //             if (geojson.geometry) {
+                //                 geojson.properties = row;
+                //             } else {
+                //                 geojson.features[i].properties = row;
+                //             }
+                //             context.data.set('map', geojson);
+                //         })
+                //         .on('rowfocus', function(row, i) {
+                //             var bounds = context.mapLayer.getBounds();
+                //             var j = 0;
+                //             context.mapLayer.eachLayer(function(l) {
+                //                 if (i === j++) smartZoom(context.map, l, bounds);
+                //             });
+                //         })
+                //     );
             }
 
         }
